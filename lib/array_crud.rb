c2cbe5b1c0954199c5ba731e8df247a32ada44ca
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 16:37:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,8 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array  = ["magenta", "cobalt blue", "green", "marigold"]
-  element = "arrays!"
-  array.pop = element
+  marigold = array.pop
 end
 
 def remove_element_from_start_of_array(array)
