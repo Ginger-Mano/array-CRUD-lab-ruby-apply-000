426a7e242cbd197fbfbe72f8ff7ae013e8db959f
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 16:48:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array  = ["wow", "cobalt blue", "green", "arrays!"]
+  array  = ["am", "cobalt blue", "green", "arrays!"]
   index_number = "#{array[0]}"
 end
 
