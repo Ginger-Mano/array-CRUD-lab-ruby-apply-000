5fb491b2c3779ee94b382aa90bdf8aca0ec95148
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 16:50:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,5 +39,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-
+  array  = ["wow", "cobalt blue", "green", "arrays!"]
+  index_number = "#{array[-1]}"
 end
