c390fcf8db6dae4fdb3f60f22046a7c1ebc4a093
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 16:19:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  colors  = ["magenta", "cobalt blue", "green", "marigold"]
-  colors << yellow
+  array  = ["magenta", "cobalt blue", "green", "marigold"]
+  element = "yellow"
+  array << "yellow"
 end
 
 def add_element_to_start_of_array(array, element)
